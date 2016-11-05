81c55a21c7b668f16f1ce41ecafcb9523267d187
Alex Aulbach <alex.aulbach@mayflower.de>
2016-11-05 17:16:29 +0100

    Finalized release.sh.

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,22 +11,23 @@ currentDir=`pwd`
 cd "$sourceDir"
 
 # json parsing for the poors
-version=`cat mod/info.json | sed '/\"version\"/!d' | sed s/\"version\":\ //g | sed s/[\",\ ]//g`
-modname=`cat mod/info.json | sed '/\"name\"/!d' | sed s/\"name\":\ //g | sed s/[\",\ ]//g`
+version=`cat mod/info.json | sed '/\"version\"/!d' | sed s/\"version\":\ //g | sed s/[^0-9\.]//g`
+modname=`cat mod/info.json | sed '/\"name\"/!d' | sed s/\"name\":\ //g | sed s/[^A-Za-z0-9-]//g`
 fullName="$modname""_$version"
 
 cd "$currentDir"
 cd "$targetDir"
 targetDir=`pwd`
 cd "$currentDir"
+targetZip="$targetDir/$fullName.zip"
 
 echo "Modname:    $modname"
 echo "Version:    $version"
 echo "Tmpdir:     $tmpdir"
-echo "Target-Zip: $targetDir/$fullName"
+echo "Target-Zip: $targetZip"
 
 cd "$sourceDir"
-git archive --prefix="$fullName" HEAD:mod | tar -C "$tmpdir" -xf -
+git archive --prefix="$fullName/" HEAD:mod | tar -C "$tmpdir" -xf -
 cp README.md "$tmpdir/$fullName"
 cd "$currentDir"
 
@@ -38,6 +39,8 @@ cd "$currentDir"
 
 # create zip
 cd "$tmpdir"
-rm "$targetDir/$fullName"
-zip -q -r "$targetDir/$fullName.zip" .
+if [ -f "$targetZip" ] ; then
+    rm "$targetDir/$fullName.zip"
+fi
+zip -q -r "$targetZip" .
 cd "$currentDir"
